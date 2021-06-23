ebff7b1b602c04aba10edcf28369737c4570094d
Garrett Gelinas <gelinas.garrett@gmail.com>
2021-06-23 20:26:02 +0100

    Fixed code errors

--- Dockerfile
+++ Dockerfile
@@ -5,15 +5,15 @@ ENV PYTHONUNBUFFERED 1
 
 # Install dependencies
 COPY ./requirements.txt /requirements.txt
-RUN apk update
 RUN apk add --update --no-cache postgresql-client
-RUN apk add --update --no-cache --virtual .tmp-build-deps gcc libc-dev linux-headers postgresql-dev
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+        gcc libc-dev linux-headers postgresql-dev
 RUN pip install -r /requirements.txt
 RUN apk del .tmp-build-deps
 
 RUN mkdir /app
 WORKDIR /app
-COPY ./app /app
+COPY ./app/ /app
 
 RUN adduser -D user
 USER user
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -6,7 +6,7 @@ from django.test import TestCase
 
 
 
-class CommandTests(TestCase):
+class CommandTestCase(TestCase):
 
     def test_wait_for_db_ready(self):
         """Test waiting for db when the db is available"""
@@ -16,7 +16,7 @@ class CommandTests(TestCase):
             self.assertEqual(gi.call_count, 1)
 
 
-    @patch('time.sleep', return_value = True)
+    @patch('time.sleep', return_value=None)
     def test_wait_for_db(self, ts):
         """Test waiting for db"""
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
