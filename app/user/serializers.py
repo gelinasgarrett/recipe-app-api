feea0125dfa098eb42c5c1d3e22f06c14c8a60e2
Garrett Gelinas <gelinas.garrett@gmail.com>
2021-07-03 18:01:12 +0100

    Added missing lines

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -37,7 +37,7 @@ class AuthTokenSerializer(serializers.Serializer):
         )
         if not user:
             msg = _('Unable to authenticate with provided credentials')
-            raise serializers.ValidationError(msg, code='authentication')
+            raise serializers.ValidationError(msg, code='authorization')
 
         attrs['user'] = user
         return attrs
